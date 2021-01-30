72f83fa3620a32685b9052c1111fe2c84247b013
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-30 20:33:54 +0900

    refactor: Add equals, hashCode method

--- app/src/main/java/com/codesoom/assignment/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.domain;
 
+import java.util.Objects;
+
 public class Task {
     private long id;
     private String title;
@@ -26,5 +28,18 @@ public class Task {
     @Override
     public String toString() {
         return String.format("Task{id=%s, title=%s}", id, title);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(title, task.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
     }
 }
